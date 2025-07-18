8c775ac34bd2eacb181b9561b8470a4b5d72cc87
Vann <vanngoh.dev@gmail.com>
2025-07-18 17:20:11 +0900

    remove redundant import

--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,4 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
